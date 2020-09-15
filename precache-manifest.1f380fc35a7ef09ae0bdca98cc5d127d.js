89c3486d275af0ec32afdb6919e1c384ec6b7fad
M <matinrosyid@live.com>
2020-09-15 20:47:42 +0100

    Updates

---
+++ precache-manifest.1f380fc35a7ef09ae0bdca98cc5d127d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb43438eced336d48dafab3a46e05ab4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12b8a1e47e3bcb7f74c0",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e1b4cfc70a6724142325",
+    "url": "/static/css/main.2e8665d3.chunk.css"
+  },
+  {
+    "revision": "12b8a1e47e3bcb7f74c0",
+    "url": "/static/js/2.073aa655.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.073aa655.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1b4cfc70a6724142325",
+    "url": "/static/js/main.eaec7184.chunk.js"
+  },
+  {
+    "revision": "df7d820ded1d4db0ff91",
+    "url": "/static/js/runtime-main.410ddc6b.js"
+  },
+  {
+    "revision": "886715e50d6cd573b76b0877653db456",
+    "url": "/static/media/background.886715e5.jpg"
+  }
+]);
